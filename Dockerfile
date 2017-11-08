704f2df36a55131d19f3b1417d529c235c711bc0
Rim-777 <javatimomoss@gmail.com>
2017-11-08 11:15:37 +0200

    changed docker image size and added params desc

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM ruby:2.4.2
+FROM ruby:2.4.2-slim
 
 RUN apt-get update
-RUN apt-get install nodejs -y
+RUN apt-get install -y build-essential git libpq-dev
 
 ENV APP_HOME /usr/src/app
 RUN mkdir $APP_HOME
--- app/api/v1/links.rb
+++ app/api/v1/links.rb
@@ -23,18 +23,27 @@ module Api
         error!(error.message, code)
       end
 
+      params do
+        requires :url, type: String, desc: 'url for shorten'
+        optional :shortcode, type: String, desc: 'desirable shortcode for shorten'
+      end
+
       desc 'Shortens some url and returns some shortcode'
       post :shorten do
         {shortcode: Link.create!(url: params[:url], shortcode: params[:shortcode]).shortcode}
       end
 
-      desc 'Gets some shortcode and returns the location for redirect'
       resource :shortcode do
+        params {requires :shortcode, type: String, desc: 'shortcode for the redirect'}
+
+        desc 'Gets some shortcode and returns the location the redirect'
         get do
           link = Link.find_by_shortcode!(params[:shortcode])
           link.increment_redirect_count!
           header('Location', link.url); status(302)
         end
+
+        params {requires :shortcode, type: String, desc: 'shortcode to get metadata'}
 
         desc "Gets a shortcode and returns url's meta data"
         get :stats do
@@ -47,7 +56,7 @@ module Api
           hide_documentation_path: true,
           info: {
               title: 'Cute-urls',
-              description: 'simple solution to shorten links'
+              description: 'Simple solution to shorten links'
           }
       )
     end
